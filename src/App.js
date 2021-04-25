7438f4d5913f097f9e7b42e3fe97a25d6138064c
Karim Makhloufi <maclouf@gmail.com>
2021-04-25 14:16:11 +0200

    Created HandleChange for each input and converted the values

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import { useState } from "react";
 import TempInput from "./Components/TempInput";
 
 function App() {
-  const [inputValue, setInputValue] = useState(0);
+  const [celciusInputValue, setCelciusInputValue] = useState(0);
+  const [farenheitInputValue, setFarenheitInputValue] = useState(32);
+
+  const handleCelciusChange = (newValue) => {
+    setCelciusInputValue(newValue);
+    setFarenheitInputValue(newValue * (9 / 5) + 32);
+  };
+
+  const handleFarenheitChange = (newValue) => {
+    setFarenheitInputValue(newValue);
+    setCelciusInputValue((newValue - 32) * (5 / 9));
+  };
   return (
     <div className="App">
       <h1>Temperature Converter</h1>
       <p>Celicius</p>
-      <TempInput inputValue={inputValue} setInputValue={setInputValue} />
+      <TempInput
+        inputValue={celciusInputValue}
+        setInputValue={handleCelciusChange}
+      />
       <p>Farenheit</p>
-      <TempInput inputValue={inputValue} setInputValue={setInputValue} />
+      <TempInput
+        inputValue={farenheitInputValue}
+        setInputValue={handleFarenheitChange}
+      />
     </div>
   );
 }
